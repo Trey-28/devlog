a5ec0548224f849467108093bf807731e2c21b63
Trey-28 <tashford@icstars.org>
2025-10-17 18:25:10 -0500

    adding my log

---
+++ script.js
@@ -0,0 +1,20 @@
+let username = "Travion";
+let number = "6"
+console.log(number );
+let numbers = 6;
+let x= number + numbers;
+let one = 1;
+let z = numbers+ one;
+console.log(z);
+
+let oneword = "travion";
+let twoword = "Moton";
+let cto = oneword + " " + twoword;
+console.log(cto);
+function add(a,b){
+let a =22;
+let b =10;
+let sum  = a +b;
+console.log(c);
+}
+add(33,22); 
